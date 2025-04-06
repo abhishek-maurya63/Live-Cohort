f3c503f2083585d08ede5fc9de4143e0d0fcb77f
abhishek maurya <maurya63abhishek@gmail.com>
2025-04-06 21:47:07 +0530

    Like feature implemented

--- Lec 44/Insta Like feature Old/script.js
+++ Lec 44/Insta Like feature Old/script.js
@@ -1,15 +1,16 @@
 let img = document.querySelector("img");
 
+let flag = 0;
 img.addEventListener("dblclick", () => {
   setTimeout(() => {
-    img.style.scale = "pop 0.3s ease-in-out forwards";
-    let flag = 1;
-    if (flag == 1) {
+    if (flag === 0) {
+      img.style.scale = "pop 0.3s ease-in-out forwards";
+      img.setAttribute("id", "liked");
       img.setAttribute("src", "liked heart.png");
-      flag = 0;
-    } else {
-      img.setAttribute("src", "heart.png");
       flag = 1;
+    } else {
+      img.setAttribute("src", "heart.png ");
+      flag = 0;
     }
   }, 500);
 });
